4fb8d2a68832fa508c145252ff00bad73a615135
ewen <ewen.grosjean@gmail.com>
2014-09-29 02:12:35 +0200

    wreak testing ...

--- src-cljs/ewen/wreak/todo_mvc.cljs
+++ src-cljs/ewen/wreak/todo_mvc.cljs
@@ -10,8 +10,24 @@
             [cljs.core.async :as async]
             [cljs.core.match])
   (:require-macros [datascript :refer [defquery query]]
-                   [cljs.core.async.macros :refer [go-loop]]
+                   [cljs.core.async.macros :refer [go go-loop]]
                    [cljs.core.match.macros :refer [match]]))
+
+(defn load-app []
+  (let [conn (ds/create-conn)]
+    (ds/transact! conn [{:db/id -1
+                         :password/label "Password1"
+                         :state/dragging false
+                         :state/sort-index 0}
+                        {:db/id -2
+                         :password/label "Password2"
+                         :state/dragging false
+                         :state/sort-index 1}
+                        {:db/id -3
+                         :view/current :home}])
+    conn))
+
+(def conn (load-app))
 
 
 (def b (component "b"
@@ -19,17 +35,31 @@
                              (html [:div#b]))}))
 
 
+(defquery get-list-passwords
+          [data] '[:find ?id
+                   :in $
+                   :where [?id :password/label ?label]] data)
+
 (def a (component "a"
-                  {:render (fn [_ _ _]
-                             (html [:div#a (b nil nil)]))}))
+                  {:render (fn [props state _]
+                             (.log js/console (str state))
+                             (html [:div#a (b nil)]))
+                   :getInitialState (fn [props db]
+                                 {:e "e"})
+                   :componentDidMount (fn []
+                                        (let [ch (async/tap (aget *component* "tx-mult") (async/chan))]
+                                          (go-loop []
+                                                   (when-let [val (async/<! ch)]
+                                                     (.log js/console val)))))}))
 
 
+(def root (w/render a {:key 1 :prop {:props "prop2"} :prop01 "prop01"}
+                    (-> (sel "#root") single-node)
+                    @conn conn))
 
-(def root (w/render (a nil nil)
-                    (-> (sel "#root") single-node)))
 
-(.log js/console "root " root)
-
-(.log js/console (coll? (transient [])))
+(js/setTimeout #(ds/transact! conn [{:db/id          1
+                                :password/label "e"}])
+               3000)
 
 
