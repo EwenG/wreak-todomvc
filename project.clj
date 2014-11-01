9d68c7c87b1d4de1c4222b63de45d1c1b18c1e8e
ewen <ewen.grosjean@gmail.com>
2014-11-01 16:46:27 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -7,17 +7,18 @@
             :source-paths ["src-cljs"]
             :resource-paths ["resources/main"]
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [org.clojure/clojurescript "0.0-2311"]
-                           #_[ewen/wreak "0.2.0-SNAPSHOT"]
+                           [org.clojure/clojurescript "0.0-2371"]
+                           [datascript "0.4.2"]
                            [sablono "0.2.6"]
                            [domina "1.0.2"]
                            [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-                           [org.clojure/core.match "0.2.1"]]
+                           [org.clojure/core.match "0.2.1"]
+                           [ewen/wreak "0.2.0-SNAPSHOT"]]
             :dev-dependencies [[lein-cljsbuild "1.0.3"]]
             :profiles {:dev {:plugins [[lein-cljsbuild "1.0.3"]]}}
+            :plugins [[com.cemerick/austin "0.1.5"]]
             :cljsbuild {:builds [{:id "dev"
-                                  :source-paths ["src-cljs" "/home/ewen/clojure/datascript/src"
-                                                 "/home/ewen/clojure/wreak/src-cljs"]
+                                  :source-paths ["src-cljs"]
                                   :compiler {
                                               :output-to "resources/dev/cljs/todo-mvc.js"
                                               :output-dir "resources/dev/cljs/"
